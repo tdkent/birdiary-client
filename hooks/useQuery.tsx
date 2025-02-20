1787aaf110a22d16dac1120f955ceb6edd506b2b
Tim Kent <timkentdev@gmail.com>
2025-02-19 17:58:34 -0800

    refactor: move fetch functions out of useEffect and correctly check session before fetching

--- hooks/useQuery.tsx
+++ hooks/useQuery.tsx
@@ -1,10 +1,6 @@
-// Determines user's auth status
-
-// useEffect determines when to fetch?
-// How to handle state?
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "@/context/AuthContext";
+import { useEffect, useState } from "react";
 import { ErrorMessages, NestResError } from "@/models/error";
+import { checkSession, getCookie } from "@/helpers/auth";
 
 type QueryParameters = {
   key: "sightings";
@@ -15,52 +11,58 @@ export default function useQuery<T>(
   key: QueryParameters["key"],
   route: QueryParameters["route"]
 ) {
-  const { isSignedIn, token } = useContext(AuthContext);
   const [fetchedData, setFetchedData] = useState<T[]>([]);
   const [error, setError] = useState<NestResError | null>(null);
   const [isPending, setIsPending] = useState(false);
 
+  // Fetch data on initial render
   useEffect(() => {
-    if (!isSignedIn) {
-      getStorageData(key);
-    } else {
-      getData(route);
-    }
-
-    async function getStorageData(key: QueryParameters["key"]) {
-      // Add default empty array to storage
-      if (!window.localStorage.getItem(key)) {
-        window.localStorage.setItem(key, "[]");
+    const checkSessionAndFetch = async () => {
+      const isLoggedIn = await checkSession();
+      if (isLoggedIn) {
+        const token = await getCookie();
+        getData(route, token!);
+      } else {
+        getStorageData(key);
       }
-      // Fetch data from local storage based on `key` parameter
-      const data: T[] = JSON.parse(window.localStorage.getItem(key)!);
-      setFetchedData(data);
-    }
+    };
 
-    async function getData(route: QueryParameters["route"]) {
-      try {
-        setIsPending(true);
-        const response = await fetch(route, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        const data: T[] | NestResError = await response.json();
-
-        if (!response.ok) {
-          setError(data as NestResError);
-        }
-
-        setFetchedData(data as T[]);
-        setIsPending(false);
-      } catch {
-        // Unexpected errors bubble to nearest error boundary
-        throw new Error(ErrorMessages.Default);
-      }
-    }
+    checkSessionAndFetch();
   }, []);
+
+  async function getStorageData(key: QueryParameters["key"]) {
+    // Add default empty array to storage
+    if (!window.localStorage.getItem(key)) {
+      window.localStorage.setItem(key, "[]");
+    }
+    // Fetch data from local storage based on `key` parameter
+    const data: T[] = JSON.parse(window.localStorage.getItem(key)!);
+    setFetchedData(data);
+  }
+
+  async function getData(route: QueryParameters["route"], token: string) {
+    try {
+      setIsPending(true);
+      const response = await fetch(route, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data: T[] | NestResError = await response.json();
+
+      if (!response.ok) {
+        setError(data as NestResError);
+      }
+
+      setFetchedData(data as T[]);
+      setIsPending(false);
+    } catch {
+      // Unexpected errors bubble to nearest error boundary
+      throw new Error(ErrorMessages.Default);
+    }
+  }
 
   return { fetchedData, error, isPending };
 }
