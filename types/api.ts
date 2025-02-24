1073792b459d1e6b89e91bfb2497c79852adf3b6
Tim Kent <timkentdev@gmail.com>
2025-02-23 16:19:13 -0800

    refactor: update request and model types

--- types/api.ts
+++ types/api.ts
@@ -1,18 +1,23 @@
 // ======= HTTP REQUESTS =======
 
-// Data for GET requests
+// Local storage key and web server route
+// Standard params for GET requests
 export type QueryParameters = {
   key: "sightings";
   route: string;
 };
 
-// Data for POST, PATCH, DELETE requests
-export type MutationParameters<T> = {
-  key: "sightings";
-  route: string;
-  method: "POST";
+// Capture all information needed for POST, PATCH, DELETE requests
+export type MutationParameters<T> = QueryParameters & {
+  method: "POST" | "PATCH" | "DELETE";
   formValues: T;
 };
+
+// Information needed for db mutation
+export type MutateDbParameters<T> = Omit<MutationParameters<T>, "key">;
+
+// Information needed for storage mutation
+export type MutateStorageParameters<T> = Omit<MutationParameters<T>, "route">;
 
 // ======= ERRORS =======
 
@@ -26,14 +31,3 @@ export type NestResError = {
 export enum ErrorMessages {
   Default = "An unexpected error occurred. Please try again later.",
 }
-
-// ======= RESPONSE BODIES =======
-
-export type RecentSighting = {
-  id: number;
-  date: Date;
-  bird: {
-    comm_name: string;
-    id: number;
-  };
-};
--- types/models.ts
+++ types/models.ts
@@ -1,3 +1,5 @@
+//! Note: naming should match database conventions
+
 // ======= BIRDS =======
 
 export type Bird = {
@@ -7,13 +9,22 @@ export type Bird = {
 
 // ======= SIGHTINGS =======
 
+// Base model
 export type Sighting = {
+  id: string;
+  user_id: string;
   bird_id: number;
-  commonName: string;
+  location_id: number;
   date: Date;
-  location?: Location;
   desc: string;
 };
+
+// Recent sighting: intersection of partial Sighting and Bird types
+export type RecentSighting = Omit<Sighting, "user_id" | "location_id"> &
+  Pick<Bird, "comm_name">;
+
+// New quick sighting data: omit "id" since it has not been created
+export type NewQuickSighting = Omit<RecentSighting, "id">;
 
 // ======= LOCATIONS =======
 
