5bc933553054e81fe83525a42ff2d91108324dba
Tim Kent <timkentdev@gmail.com>
2025-03-18 21:52:34 -0700

    feat: add modal component

---
+++ components/ui/Modal.tsx
@@ -0,0 +1,45 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+type ModalProps = {
+  triggerText: string;
+  title: string;
+  description: string;
+  children: React.ReactNode;
+};
+
+export default function Modal({
+  triggerText,
+  title,
+  description,
+  children,
+}: ModalProps) {
+  return (
+    <Dialog>
+      <DialogTrigger>{triggerText}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        {children}
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Cancel
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
