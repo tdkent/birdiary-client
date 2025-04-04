bbc1ea4fd17add1145f2b64fb0aa3e6de2816c17
Tim Kent <timkentdev@gmail.com>
2025-04-03 20:56:56 -0700

    refactor: remove sightings page and subpages

--- app/sightings/all/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AllSightingsView() {
-  return (
-    <div>
-      <h1>All Sightings</h1>
-      <p>Show all user&apos;s sightings</p>
-    </div>
-  );
-}
--- app/sightings/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SightingsByBirdView() {
-  return (
-    <div>
-      <h1>My Birds</h1>
-      <p>List all sightings grouped by bird</p>
-    </div>
-  );
-}
