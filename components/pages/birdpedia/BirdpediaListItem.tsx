4a04329c51c1f9266acd182bd3c85fb721191105
Tim Kent <timkentdev@gmail.com>
2025-04-14 20:27:17 -0700

    feat: add crosslink from bird list item to bird details page

--- components/pages/birdpedia/BirdpediaListItem.tsx
+++ components/pages/birdpedia/BirdpediaListItem.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import type { SingleBirdWithCount } from "@/types/models";
 
 type BirdpediaListItemProps = {
@@ -7,15 +8,17 @@ export default function BirdpediaListItem({ bird }: BirdpediaListItemProps) {
   return (
     <>
       <li className="my-4">
-        <h3 className="text-base">{bird.commName}</h3>
-        <div className="flex gap-4 text-xs">
-          <span>
-            <i>{bird.sciName}</i>
-          </span>
-          <span>
-            {bird.count && bird.count >= 1 ? `${bird.count} sightings` : null}
-          </span>
-        </div>
+        <Link href={`/birds/${bird.commName.replace(" ", "_")}`}>
+          <h3 className="text-base">{bird.commName}</h3>
+          <div className="flex gap-4 text-xs">
+            <span>
+              <i>{bird.sciName}</i>
+            </span>
+            <span>
+              {bird.count && bird.count >= 1 ? `${bird.count} sightings` : null}
+            </span>
+          </div>
+        </Link>
       </li>
     </>
   );
