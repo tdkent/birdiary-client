f1ce2194413a572b99e9d597add9343a3e0dcb87
Tim Kent <timkentdev@gmail.com>
2025-05-09 21:19:58 -0700

    feat: add update password form and schema

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,9 +1,15 @@
+import UpdatePasswordForm from "@/components/forms/UpdatePasswordForm";
 export default function AccountView() {
   return (
-    <div>
-      <h1>Account</h1>
-      <p>Display and update sensitive user info</p>
-      <p>Email, password, 2fa, etc</p>
-    </div>
+    <>
+      <header className="flex flex-col gap-4">
+        <h1>Account</h1>
+        <p>View and edit your account information.</p>
+      </header>
+      <section>
+        <h2>Update Password</h2>
+        <UpdatePasswordForm />
+      </section>
+    </>
   );
 }
