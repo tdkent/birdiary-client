8a93489b280b592f0cad3d27bbf04dbce0bb2b43
Tim Kent <timkentdev@gmail.com>
2025-05-12 20:08:54 -0700

    feat: add user email to account view

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,5 +1,23 @@
+import { getUser } from "@/actions/profile";
+import type { UserProfile } from "@/types/models";
+import type { ExpectedServerError } from "@/types/api";
+import ErrorDisplay from "@/components/pages/shared/ErrorDisplay";
 import UpdatePasswordForm from "@/components/forms/UpdatePasswordForm";
-export default function AccountView() {
+export default async function AccountView() {
+  const user: UserProfile | ExpectedServerError = await getUser();
+
+  if ("error" in user) {
+    const msg = Array.isArray(user.message)
+      ? user.message.join(",")
+      : user.message;
+
+    return (
+      <>
+        <ErrorDisplay msg={`${user.error}: ${msg}`} />
+      </>
+    );
+  }
+
   return (
     <>
       <header className="flex flex-col gap-4">
@@ -7,7 +25,9 @@ export default function AccountView() {
         <p>View and edit your account information.</p>
       </header>
       <section>
-        <h2>Update Password</h2>
+        <h2>Email &amp; Password</h2>
+        <p>Email: {user.email}</p>
+        <h3>Update Password</h3>
         <UpdatePasswordForm />
       </section>
     </>
