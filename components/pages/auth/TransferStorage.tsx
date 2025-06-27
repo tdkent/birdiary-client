1b8115e8ed980d665cde9c4c2a82dd0845a1388e
Tim Kent <timkentdev@gmail.com>
2025-06-26 20:42:25 -0700

    feat: add shadcn popover component and tooltip with info:

--- components/pages/auth/TransferStorage.tsx
+++ components/pages/auth/TransferStorage.tsx
@@ -1,4 +1,10 @@
 import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Info } from "lucide-react";
 import { FormLabel } from "@/components/ui/form";
 import type { Sighting } from "@/types/models";
 
@@ -16,11 +22,21 @@ export default function TransferStorage({ pathname }: TransferStorageType) {
   if (!parsedSightings.length) return null;
 
   return (
-    <div className="flex items-center space-x-2">
+    <div className="flex items-center space-x-3">
       <Checkbox />
       <FormLabel>
         Transfer sighting data from browser to your account?
       </FormLabel>
+      <Popover>
+        <PopoverTrigger>
+          <Info strokeWidth={1.5} size={20} />
+        </PopoverTrigger>
+        <PopoverContent>
+          Your browser contains sightings data created when you were logged out
+          of your account. By checking this box when signing in, this data will
+          be transferred to your account and deleted from your browser.
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
