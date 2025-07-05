a552c00aec8f8442b966a63b6695d44a4b017515
Tim Kent <timkentdev@gmail.com>
2025-07-05 11:39:21 -0700

    refactor: update auth routes

--- actions/auth.ts
+++ actions/auth.ts
@@ -2,20 +2,17 @@
 
 import { redirect } from "next/navigation";
 import { createSession, deleteSession } from "@/lib/session";
-import { BASE_URL } from "@/constants/env";
 import type { AuthParams, AuthResponse } from "@/types/auth";
 import { ExpectedServerError, ErrorMessages } from "@/types/api";
+import { apiRoutes } from "@/types/api";
 
-// This function handles both 'Sign up' and 'Sign in' auth actions
-// The request body contains the email and password in both cases
-// The API endpoint is determined based on pathname from params
-// ...args is a rest parameter of type { email:string; password: string }
+/** Sign up or sign in a user */
 export async function auth({ pathname, ...args }: AuthParams) {
   let endpoint: string;
   if (pathname === "/signup") {
-    endpoint = BASE_URL + "/users";
+    endpoint = apiRoutes.signup;
   } else {
-    endpoint = BASE_URL + "/users/auth/signin";
+    endpoint = apiRoutes.signin;
   }
 
   try {
@@ -29,13 +26,11 @@ export async function auth({ pathname, ...args }: AuthParams) {
 
     const data: ExpectedServerError | AuthResponse = await response.json();
 
-    // Return expected error object containing error property
     if (!response.ok) {
       return data as ExpectedServerError;
     }
 
     if (pathname === "/signin") {
-      // Use type guard to check that the id is in the response object
       if ("id" in data) {
         await createSession(data.id);
       } else {
