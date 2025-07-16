c9b97591fe9fb37d139c3a0d16616d1acf6ae70b
Tim Kent <timkentdev@gmail.com>
2025-07-16 09:46:54 -0700

    refactor: add new text content and links

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,59 @@
-import { caveat } from "@/lib/fonts";
-import QuickSightingForm from "@/components/forms/QuickSightingForm";
-import DetailedSightingModal from "@/components/pages/home/DetailedSightingModal";
-import CsrList from "@/components/pages/shared/CsrList";
-import { apiRoutes } from "@/models/api";
+import { quicksand } from "@/lib/fonts";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Slideshow from "@/components/pages/home/Slideshow";
 
 export default function Home() {
   return (
     <div>
       <header>
-        <h1 className={`${caveat.className}`}>Welcome to Birdiary!</h1>
-        <p className={`${caveat.className} mt-4 text-2xl`}>
-          Quickly log your sightings.
-        </p>
-        <p className={`${caveat.className} text-2xl`}>
-          Build your birding diary.
-        </p>
+        <h1 className={`${quicksand.className}`}>
+          Quickly log your bird sightings and build your birding diary
+        </h1>
+        <h2 className={`${quicksand.className}`}>
+          Birdiary is a free website. An account is not required.
+        </h2>
       </header>
-      <section className="my-6 rounded border p-4">
-        <div className="">
-          <h3>Spot a bird? Add it fast!</h3>
-          <QuickSightingForm />
-        </div>
-        <DetailedSightingModal />
+      <Slideshow />
+      <section>
+        <h3>Create your birding diary</h3>
+        <p>
+          Spot a bird? Pick from our database of more than 800 North American
+          bird species, and log the sighting details.
+        </p>
+        <p>
+          Your sightings will be used to create your birding diary, tracking
+          your sightings by date and species.
+        </p>
+        <h3>Go further with an account</h3>
+        <p>
+          Interested in more? Create a free account to permanently store
+          unlimited <span className="after:content-['*']">data</span>, keep
+          track of your birdwatching life list, and add location data to your
+          sightings.{" "}
+          <Link href="/plans" className="underline hover:no-underline">
+            View plans
+          </Link>{" "}
+          to learn more.
+        </p>
       </section>
-      <section className="my-4">
-        <h2>Your Recent Sightings</h2>
-        <CsrList
-          route={apiRoutes.sightings}
-          tag="sightings"
-          variant="recentSighting"
-        />
+      <section>
+        <h3>Ready to get started?</h3>
+        <Button asChild className="my-2 w-full p-6 text-lg text-foreground">
+          <Link href="/new">
+            <Plus />
+            Add a new sighting
+          </Link>
+        </Button>
+      </section>
+      <section className="mt-16">
+        <p className="rounded border p-4 text-sm before:content-['*']">
+          If you do not have an account or are not signed in, your sighting and
+          diary data is stored in your browser and not our database. This limits
+          the total amount of data you can store, and your data may be lost if
+          your browser&apos;s data cache is deleted.
+        </p>
       </section>
     </div>
   );
