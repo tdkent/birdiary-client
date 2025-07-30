4c094a8108121956798e360d15063259fa8e4e4a
Tim Kent <timkentdev@gmail.com>
2025-07-30 13:20:26 -0700

    refactor: use map to render list skeleton

--- components/pages/shared/Pending.tsx
+++ components/pages/shared/Pending.tsx
@@ -11,15 +11,10 @@ export default function Pending({ variant, listSize }: PendingProps) {
     case "list": {
       return (
         <>
-          <div className="mt-8 flex flex-col gap-6">
-            <Skeleton className="h-10 w-1/2" />
-            <Skeleton className="h-14 w-full" />
-            <div className="flex flex-col gap-4">
-              {new Array(listSize).fill(null).map((_, i) => {
-                return <Skeleton key={i} className="h-5 w-full" />;
-              })}
-            </div>
-            <Skeleton className="mx-auto h-12 w-3/4" />
+          <div className="mb-4 mt-8 flex flex-col gap-4">
+            {new Array(listSize).fill(null).map((_, i) => {
+              return <Skeleton key={i} className="h-5 w-full" />;
+            })}
           </div>
         </>
       );
