31d46f716f5cc2b43c8ed10e941bffd4cc9f93e5
Tim Kent <timkentdev@gmail.com>
2025-08-01 20:52:22 -0700

    feat: add server action to fetch sighting data

---
+++ actions/sighting.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { getCookie } from "@/helpers/auth";
+import { apiRoutes, Messages } from "@/models/api";
+
+export async function getSighting(id: number) {
+  try {
+    const token = await getCookie();
+    const response = await fetch(apiRoutes.sighting(id), {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
+    throw new Error(Messages.DefaultError);
+  }
+}
