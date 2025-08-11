e3a787a58980f3026f157072c0ab6f86868e8db6
Tim Kent <timkentdev@gmail.com>
2025-08-11 10:22:47 -0700

    feat: fetch and display data from storage when not logged in

--- components/pages/sightings/Sighting.tsx
+++ components/pages/sightings/Sighting.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Link from "next/link";
 import { useToast } from "@/hooks/use-toast";
+import { AuthContext } from "@/context/AuthContext";
 import {
   apiRoutes,
   Messages,
   ServerResponseWithError,
   ServerResponseWithObject,
 } from "@/models/api";
-import type { SightingWithLocation } from "@/models/display";
-import { getCookie } from "@/helpers/auth";
+import type { SightingInStorage, SightingWithLocation } from "@/models/display";
 import BirdImage from "@/components/forms/BirdImage";
 import { createLocaleString } from "@/helpers/dates";
 
@@ -20,20 +20,23 @@ type SightingProps = {
 
 /** Fetch and display sighting data. */
 export default function Sighting({ sightingId }: SightingProps) {
-  const [data, setData] = useState<SightingWithLocation | null>(null);
+  const { token } = useContext(AuthContext);
+  const [data, setData] = useState<
+    SightingWithLocation | SightingInStorage | null
+  >(null);
   const [error, setError] = useState<string | null>(null);
   const [pending, setPending] = useState(false);
 
   const { toast } = useToast();
+  const route = apiRoutes.sighting(sightingId);
 
   useEffect(() => {
     async function query() {
       setError(null);
-      const token = await getCookie();
       if (token) {
         setPending(true);
         try {
-          const response = await fetch(apiRoutes.sighting(sightingId), {
+          const response = await fetch(route, {
             headers: { Authorization: `Bearer ${token}` },
           });
 
@@ -60,13 +63,20 @@ export default function Sighting({ sightingId }: SightingProps) {
           setPending(false);
         }
       } else {
-        // const { items, countOfRecords } = queryStorage(route, tag);
-        // setData((items as SightingInStorage[] | Group[]) || []);
-        // setCount(countOfRecords);
+        if (!window.localStorage.getItem("sightings")) {
+          window.localStorage.setItem("sightings", "[]");
+        }
+        const data = JSON.parse(
+          window.localStorage.getItem("sightings")!,
+        ) as SightingInStorage[];
+        const sighting = data.find((s) => s.id === sightingId);
+        // TODO: handle if no sighting
+        if (!sighting) return;
+        setData(sighting);
       }
     }
     query();
-  }, [sightingId]);
+  }, [route, sightingId, token]);
 
   useEffect(() => {
     if (error) {
@@ -84,6 +94,42 @@ export default function Sighting({ sightingId }: SightingProps) {
 
   if (!data || pending) {
     return <p>Loading...</p>;
+  }
+
+  if (!token) {
+    const { bird, date, description } = data as SightingInStorage;
+    return (
+      <>
+        <section>
+          <BirdImage currBirdName={bird.commonName} />
+          <h2>{bird.commonName}</h2>
+          <dl className="my-8 flex flex-col gap-6">
+            <div>
+              <dt className="text-xs font-semibold uppercase">Date</dt>
+              <dd className="">{createLocaleString(date, "full")}</dd>
+              <Link
+                href={`/diary/${date.slice(0, 10)}`}
+                className="text-sm italic hover:underline"
+              >
+                View diary
+              </Link>
+            </div>
+            <div>
+              <dt className="text-xs font-semibold uppercase">Description</dt>
+              {description ? (
+                <>
+                  <dd>{description}</dd>
+                </>
+              ) : (
+                <>
+                  <dd className="italic">No description provided</dd>
+                </>
+              )}
+            </div>
+          </dl>
+        </section>
+      </>
+    );
   }
 
   const { bird, date, description, location } = data as SightingWithLocation;
