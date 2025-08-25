86c0fadd81cc9d1211443fb57f9e308826f6f1f9
Tim Kent <timkentdev@gmail.com>
2025-08-25 11:40:18 -0700

    feat: add new logo images

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -1,15 +1,24 @@
 import Link from "next/link";
 import Image from "next/image";
 
+const logoImgs = [
+  "Blue-Jay",
+  "Emperor-Goose",
+  "Northern-Cardinal",
+  "Painted-Bunting",
+];
+
 /** Birdiary logo and header text with link to Home */
 export default function Logo() {
+  const selectedImg = logoImgs[Math.floor(Math.random() * 4)];
+  const imgAlt = selectedImg.split("-").join(" ");
   return (
     <>
       <Link href="/" className="flex items-center gap-3 md:gap-4">
-        <div className="relative h-14 w-14 rounded-full border-2 border-blue-300 sm:h-16 sm:w-16">
+        <div className="relative h-14 w-14 rounded-full border border-foreground sm:h-16 sm:w-16">
           <Image
-            src="/icon/blue-jay-icon.webp"
-            alt="Blue Jay"
+            src={`/icon/${selectedImg}.webp`}
+            alt={imgAlt}
             fill
             sizes="(max-width: 768px) 48px, 64px"
             className="rounded-full object-cover"
