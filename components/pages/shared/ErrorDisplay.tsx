276436fcf8b779af8185a6c50904cbfb1af64d5b
Tim Kent <timkentdev@gmail.com>
2025-08-26 17:00:56 -0700

    style: update error displays

--- components/pages/shared/ErrorDisplay.tsx
+++ components/pages/shared/ErrorDisplay.tsx
@@ -1,6 +1,3 @@
-// Render client error information
-// Use as an alternate to an error boundary
-// Optionally loads a button to reload the page
 import { CircleAlert } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
@@ -9,6 +6,7 @@ type ErrorDisplayProps = {
   showReloadBtn?: boolean;
 };
 
+/** Show error information and optional reload button */
 export default function ErrorDisplay({
   msg,
   showReloadBtn,
@@ -16,14 +14,16 @@ export default function ErrorDisplay({
   const reload = () => location.reload();
   return (
     <>
-      <div className="my-8 flex flex-col gap-2 rounded-md border border-destructive p-4">
-        <span className="flex items-center gap-2 text-xl font-semibold text-destructive">
-          <CircleAlert />
+      <div className="my-8 flex flex-col gap-2 rounded-md border border-destructive p-4 md:w-[85%]">
+        <span className="flex items-center gap-2 text-xl font-semibold text-destructive md:gap-3 md:text-2xl">
+          <CircleAlert strokeWidth={1.5} size={28} />
           An error occurred
         </span>
         <dl className="my-8 flex flex-col gap-8 px-2">
           <div className="flex flex-col gap-1">
-            <dt className="text-sm font-semibold uppercase">Message</dt>
+            <dt className="text-sm font-semibold uppercase md:text-base">
+              Message
+            </dt>
             <dd className="text-xl">{msg}</dd>
           </div>
         </dl>
