933764ac43159e6e3ac4ff30a1c1a6fa41937aa4
Tim Kent <timkentdev@gmail.com>
2025-09-07 13:33:28 -0700

    refactor: update diary view to validate url queries and render error

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -8,6 +8,8 @@ import {
   sortBySightingsCount,
 } from "@/models/form";
 import { apiRoutes } from "@/models/api";
+import { checkValidParamInteger } from "@/helpers/data";
+import ErrorDisplay from "@/components/pages/shared/ErrorDisplay";
 
 export default async function DiaryView({
   searchParams,
@@ -15,20 +17,33 @@ export default async function DiaryView({
   searchParams: Promise<{ [key: string]: string | undefined }>;
 }) {
   const { page, sortBy } = await searchParams;
-  const sortOptions = [...sortByDateOptions, sortBySightingsCount];
-  const parsedPage = Number(page);
 
-  if (
-    !page ||
-    !sortBy ||
-    !parsedPage ||
-    parsedPage < 1 ||
-    !sortOptions.find((option) => option.value === sortBy)
-  ) {
-    redirect(`/diary?page=1&sortBy=dateDesc`);
+  if (!page || !sortBy) {
+    redirect(`/diary?page=${page || "1"}&sortBy=${sortBy || "dateDesc"}`);
   }
 
+  const parsedPage = checkValidParamInteger(page);
+  const sortOptions = [...sortByDateOptions, sortBySightingsCount];
   const defaultSortOption = sortBy as SortValues;
+
+  if (
+    !parsedPage ||
+    (sortOptions && !sortOptions.find((option) => option.value === sortBy))
+  ) {
+    return (
+      <>
+        <ViewWrapper>
+          <ViewHeader
+            headingText="Life List"
+            descriptionText="View your complete North American birdwatching life list."
+            backLinkHref="sightings"
+            backLinkText="Go to sightings"
+          />
+          <ErrorDisplay msg="Invalid request." />
+        </ViewWrapper>
+      </>
+    );
+  }
 
   return (
     <>
@@ -38,7 +53,7 @@ export default async function DiaryView({
           descriptionText="View a list of your sightings grouped by date."
         />
         <CsrList
-          route={apiRoutes.getSightingsGroupByType("date", parsedPage, sortBy)}
+          route={apiRoutes.getSightingsGroupByType("date", page, sortBy)}
           variant="diary"
           pendingVariant="listSingleRow"
           tag="diary"
