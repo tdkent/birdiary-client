61997f083c849f2a182520001c0137284383bbac
Tim Kent <timkentdev@gmail.com>
2025-09-09 15:31:48 -0700

    refactor: add inline error variant to error display for use with forms

--- components/pages/shared/ErrorDisplay.tsx
+++ components/pages/shared/ErrorDisplay.tsx
@@ -2,11 +2,15 @@ import { CircleAlert } from "lucide-react";
 import { Messages } from "@/models/api";
 
 type ErrorDisplayProps = {
+  showInline?: boolean;
   statusCode?: number | string;
 };
 
 /** Show error information and optional reload button */
-export default function ErrorDisplay({ statusCode }: ErrorDisplayProps) {
+export default function ErrorDisplay({
+  showInline,
+  statusCode,
+}: ErrorDisplayProps) {
   const parseStatusCode = Number(statusCode);
   let errorMessage = "";
   switch (parseStatusCode) {
@@ -28,9 +32,27 @@ export default function ErrorDisplay({ statusCode }: ErrorDisplayProps) {
     }
   }
 
+  if (showInline) {
+    return (
+      <>
+        <div className="rounded-md border border-destructive p-4 md:w-3/4">
+          <span className="text-lg text-destructive">
+            <CircleAlert
+              strokeWidth={1.5}
+              size={20}
+              className="mr-2 inline-flex -translate-y-0.5"
+            />
+
+            {errorMessage}
+          </span>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
-      <div className="my-8 flex flex-col gap-2 rounded-md border border-destructive p-4 md:w-[85%]">
+      <div className="my-8 flex flex-col gap-2 rounded-md border border-destructive p-4 md:w-3/4">
         <span className="flex items-center gap-2 text-xl font-semibold text-destructive md:gap-3 md:text-2xl">
           <CircleAlert strokeWidth={1.5} size={28} />
           An error occurred
