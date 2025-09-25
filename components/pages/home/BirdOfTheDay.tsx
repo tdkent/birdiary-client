197e0fc8607f4a95dbb88bb570d9cf34fa56f8ed
Tim Kent <timkentdev@gmail.com>
2025-09-25 12:46:40 -0700

    feat: add birdoftheday api route and update component to fetch bird from it

--- components/pages/home/BirdOfTheDay.tsx
+++ components/pages/home/BirdOfTheDay.tsx
@@ -1,17 +1,22 @@
-import Image from "next/image";
+import BirdImage from "@/components/forms/BirdImage";
+import ErrorDisplay from "@/components/pages/shared/ErrorDisplay";
+import { apiRoutes, ExpectedServerError } from "@/models/api";
+import { Bird } from "@/models/db";
 
-export default function BirdOfTheDay() {
+/** Bird display updated daily. */
+export default async function BirdOfTheDay() {
+  const response = await fetch(apiRoutes.birdOfTheDay);
+  const result: Bird | ExpectedServerError = await response.json();
+  if ("error" in result) {
+    return <ErrorDisplay statusCode={result.statusCode} />;
+  }
   return (
     <section className="my-4 flex flex-col gap-2 rounded-md border p-2">
       <h3>Bird of the Day</h3>
-      <div className="relative aspect-[5/3] w-full border">
-        <Image
-          src="https://res.cloudinary.com/dwce3mvbj/image/upload/v1730219480/Great_Blue_Heron_audjs7.jpg"
-          alt="Great Blue Heron"
-          fill
-          className="rounded-md object-fill"
-        />
-      </div>
+      <BirdImage
+        currBirdName={result.commonName}
+        sizes="(max-width: 1024px) 100vw, 780px"
+      />
       <p className="text-sm">Your sightings: No sightings yet!</p>
     </section>
   );
