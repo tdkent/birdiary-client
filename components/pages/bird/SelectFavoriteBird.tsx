c494307addd68f803d4f41beeae4183545d1a5b5
Tim Kent <timdekent@gmail.com>
2025-10-02 14:41:15 -0700

    feat: set patch request on button click and handle expected errors

--- components/pages/bird/SelectFavoriteBird.tsx
+++ components/pages/bird/SelectFavoriteBird.tsx
@@ -1,11 +1,36 @@
-import { Button } from "@/components/ui/button";
-import { Heart } from "lucide-react";
+"use client";
 
-export default function SelectFavoriteBird() {
+import { updateFavoriteBird } from "@/actions/profile";
+import ErrorDisplay from "@/components/pages/shared/ErrorDisplay";
+import { Button } from "@/components/ui/button";
+import { ExpectedServerError } from "@/models/api";
+import { User } from "@/models/db";
+import { Heart } from "lucide-react";
+import { useState } from "react";
+
+type SelectFavoriteBirdType = {
+  birdId: number;
+};
+
+export default function SelectFavoriteBird({ birdId }: SelectFavoriteBirdType) {
+  const [error, setError] = useState<number | null>(null);
+  const handleClick = async () => {
+    setError(null);
+    const result: User | ExpectedServerError = await updateFavoriteBird(birdId);
+    if ("error" in result) {
+      return setError(result.statusCode);
+    }
+  };
   return (
     <>
-      <div className="flex items-center rounded-md">
-        <Button className="text-base font-normal" variant="outline" size="lg">
+      <div className="flex flex-col gap-4">
+        {error && <ErrorDisplay showInline statusCode={error} />}
+        <Button
+          className="text-base font-normal"
+          onClick={handleClick}
+          variant="outline"
+          size="lg"
+        >
           <Heart size={18} strokeWidth={1.5} />
           Set favorite bird
         </Button>
