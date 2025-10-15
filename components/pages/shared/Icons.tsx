eaae1a4bcc71a49d1bec12a073d0ebfdf43c3532
Tim Kent <timkentdev@gmail.com>
2025-10-15 14:57:33 -0700

    feat: add icons component to display bird image thumbnails

---
+++ components/pages/shared/Icons.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+
+type IconsProps = {
+  sightings: string[];
+};
+
+export default function Icons({ sightings }: IconsProps) {
+  return (
+    <>
+      <div className="flex">
+        {sightings.map((sighting) => {
+          const [sightingId, commonName, imgSecureUrl] = sighting.split(",");
+          return (
+            <React.Fragment key={sightingId}>
+              <div className="relative aspect-square w-14 overflow-hidden rounded-full border border-black">
+                <Image
+                  alt={commonName}
+                  className="object-cover"
+                  fill
+                  src={imgSecureUrl}
+                />
+              </div>
+            </React.Fragment>
+          );
+        })}
+      </div>
+    </>
+  );
+}
