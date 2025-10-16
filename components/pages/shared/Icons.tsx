139f1897f052dd5543aacec2a072f6ae81a620b4
Tim Kent <timkentdev@gmail.com>
2025-10-16 14:06:31 -0700

    style: improve adaptive layout of list items

--- components/pages/shared/Icons.tsx
+++ components/pages/shared/Icons.tsx
@@ -10,24 +10,27 @@ import React from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 type IconsProps = {
-  count: number;
-  sightings: string[];
+  count: number | undefined;
+  sightings: string[] | undefined;
 };
 
 export default function Icons({ count, sightings }: IconsProps) {
   const matches = useMediaQuery("(min-width:640px)");
+
+  if (!count || !sightings) return null;
+
   const iconsToShow = matches
     ? DESKTOP_REMAINING_COUNT
     : MOBILE_REMAINING_COUNT;
   const remainingCount = count - iconsToShow;
   return (
     <>
-      <div className="flex pr-4 sm:gap-1 lg:gap-2">
+      <div className="flex grow justify-end pr-4 sm:gap-1 md:w-3/5 md:grow-0 lg:gap-2">
         {sightings.slice(0, iconsToShow).map((sighting) => {
           const [sightingId, commonName, imgSecureUrl] = sighting.split(",");
           return (
             <React.Fragment key={sightingId}>
-              <div className="relative ml-[-20px] aspect-square w-14 overflow-hidden rounded-full bg-background shadow-md shadow-gray-800 dark:shadow-gray-900 sm:m-0 md:w-16">
+              <div className="relative ml-[-20px] aspect-square w-14 overflow-hidden rounded-full bg-background shadow-md shadow-gray-800 dark:shadow-gray-900 sm:ml-[-10px] md:w-16">
                 <Image
                   alt={commonName}
                   className="object-cover"
@@ -41,7 +44,7 @@ export default function Icons({ count, sightings }: IconsProps) {
         })}
         {remainingCount >= 1 && (
           <>
-            <div className="relative ml-[-20px] flex aspect-square w-14 items-center justify-center overflow-hidden rounded-full bg-gray-100 shadow-md shadow-gray-800 dark:bg-blue-950 dark:shadow-gray-900 sm:m-0 md:w-16">
+            <div className="relative ml-[-20px] flex aspect-square w-14 items-center justify-center overflow-hidden rounded-full bg-gray-100 shadow-md shadow-gray-800 dark:bg-blue-950 dark:shadow-gray-900 sm:ml-[-10px] md:w-16">
               <p className="flex items-center gap-1 text-2xl text-foreground">
                 <Plus className="m-[-6px]" strokeWidth={2} size={16} />
                 <span className="font-numbers">{remainingCount}</span>
--- components/pages/shared/ListItemNEW.tsx
+++ components/pages/shared/ListItemNEW.tsx
@@ -1,10 +1,12 @@
 import Icons from "@/components/pages/shared/Icons";
+import SightingBadge from "@/components/pages/shared/SightingBadge";
 import Link from "next/link";
 import type { ReactNode } from "react";
 
 type ListItemProps = {
   count?: number;
   href: string;
+  isNew?: boolean;
   mainText: string;
   subText: string | ReactNode;
   sightings?: string[];
@@ -13,25 +15,28 @@ type ListItemProps = {
 export default function ListItemNEW({
   count,
   href,
+  isNew,
   mainText,
   sightings,
   subText,
 }: ListItemProps) {
-  const showIcons = sightings && count;
   return (
     <>
       <li className="list-hover hover:scale-[1.025]">
         <Link href={href}>
           <div className="flex h-[72px] items-center justify-between md:h-20">
-            <div className="flex flex-col px-4 md:w-2/5 md:flex-row md:items-center">
-              <span className="line-clamp-1 text-base font-semibold md:grow md:text-xl">
-                {mainText}
-              </span>
-              <span className="line-clamp-1 flex items-center gap-0.5 pr-0.5 text-xs italic md:shrink-0 md:text-base">
+            <div className="flex flex-col pl-4 max-md:gap-1.5 md:grow md:flex-row md:items-center md:justify-between">
+              <div className="flex items-center gap-4 md:gap-6 lg:gap-8">
+                <span className="line-clamp-1 text-base font-semibold md:grow md:text-xl">
+                  {mainText}
+                </span>
+                <SightingBadge isNew={isNew} />
+              </div>
+              <span className="line-clamp-1 flex items-center gap-0.5 pr-0.5 text-sm italic md:shrink-0 md:text-base">
                 {subText}
               </span>
             </div>
-            {showIcons && <Icons count={count} sightings={sightings} />}
+            <Icons count={count} sightings={sightings} />
           </div>
         </Link>
       </li>
