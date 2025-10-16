e196be13b83b29afbd9da3a5b0e2eb93560ec193
Tim Kent <timkentdev@gmail.com>
2025-10-15 20:02:10 -0700

    style: add icon shadows

--- components/pages/shared/Icons.tsx
+++ components/pages/shared/Icons.tsx
@@ -8,12 +8,12 @@ type IconsProps = {
 export default function Icons({ sightings }: IconsProps) {
   return (
     <>
-      <div className="flex">
+      <div className="flex md:gap-1">
         {sightings.map((sighting) => {
           const [sightingId, commonName, imgSecureUrl] = sighting.split(",");
           return (
             <React.Fragment key={sightingId}>
-              <div className="relative ml-[-16px] aspect-square w-14 overflow-hidden rounded-full border border-black">
+              <div className="relative ml-[-20px] aspect-square w-14 overflow-hidden rounded-full shadow-md shadow-gray-800 dark:shadow-gray-900 md:m-0 md:w-16">
                 <Image
                   alt={commonName}
                   className="object-cover"
