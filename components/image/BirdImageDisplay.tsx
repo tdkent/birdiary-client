5f1776fec38848623039cff2eb43f73848ab5f7d
Tim Kent <timkentdev@gmail.com>
2025-10-30 10:27:34 -0700

    feat: wrap figure with dialog

--- components/image/BirdImageDisplay.tsx
+++ components/image/BirdImageDisplay.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Bird } from "@/models/db";
 import { Bird as BirdIcon } from "lucide-react";
 import Image from "next/image";
@@ -20,23 +28,50 @@ export default function BirdImageDisplay({
   const { commonName, imgAttribute, scientificName } = bird;
   return (
     <>
-      <figure className="flex flex-col gap-1 md:gap-2">
-        <div className="relative flex aspect-[5/3] w-full items-center justify-center gap-2 overflow-hidden rounded-md border">
-          {loading ? <BirdIcon strokeWidth={1} size={64} /> : null}
-          <Image
-            alt={commonName}
-            className="object-cover"
-            fill
-            onLoad={() => setLoading(false)}
-            priority
-            sizes={sizes}
-            src={imgUrl}
-          />
-        </div>
-        <figcaption className="px-1 text-xs italic md:text-sm">
-          {scientificName}. &copy; {imgAttribute ?? "Public Domain"}
-        </figcaption>
-      </figure>
+      <Dialog>
+        <DialogTrigger>
+          <figure className="flex flex-col gap-1 md:gap-2">
+            <div className="relative flex aspect-[5/3] w-full items-center justify-center gap-2 overflow-hidden rounded-md border">
+              {loading ? <BirdIcon strokeWidth={1} size={64} /> : null}
+              <Image
+                alt={commonName}
+                className="object-cover"
+                fill
+                onLoad={() => setLoading(false)}
+                priority
+                sizes={sizes}
+                src={imgUrl}
+              />
+            </div>
+            <figcaption className="px-1 text-xs italic md:text-sm">
+              {scientificName}. &copy; {imgAttribute ?? "Public Domain"}
+            </figcaption>
+          </figure>
+        </DialogTrigger>
+        <DialogContent className="min-w-[95%]">
+          <DialogHeader className="sr-only">
+            <DialogTitle>{bird.commonName}</DialogTitle>
+            <DialogDescription>
+              Full-screen image of {bird.commonName}
+            </DialogDescription>
+          </DialogHeader>
+          <figure className="flex flex-col gap-1 md:gap-2">
+            <div className="relative flex aspect-[5/3] w-full items-center justify-center gap-2 overflow-hidden rounded-md border">
+              {loading ? <BirdIcon strokeWidth={1} size={64} /> : null}
+              <Image
+                alt={commonName}
+                className="object-cover"
+                fill
+                onLoad={() => setLoading(false)}
+                priority
+                quality={100}
+                sizes={sizes}
+                src={imgUrl}
+              />
+            </div>
+          </figure>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
