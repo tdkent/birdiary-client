df5b620a02119d8ee62c5902f60d8ca858c0806a
Tim Kent <timkentdev@gmail.com>
2025-11-17 13:59:45 -0800

    refactor: add support email address to env variables

--- constants/constants.ts
+++ constants/constants.ts
@@ -9,5 +9,3 @@ export const FREE_TEXT_LENGTH = 150;
 // Sighting Icons
 export const MOBILE_REMAINING_COUNT = 3;
 export const DESKTOP_REMAINING_COUNT = MOBILE_REMAINING_COUNT + 1;
-// Email
-export const SUPPORT_EMAIL_ADDRESS = "support@mybirdiary.com";
