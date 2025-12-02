ec648e159f88810cacdbf6ef04211cc557452691
Tim Kent <timkentdev@gmail.com>
2025-12-01 22:39:01 -0800

    fix: prevent redundant api call in diary detail view if date id is invalid

--- app/diary/[id]/page.tsx
+++ app/diary/[id]/page.tsx
@@ -19,14 +19,20 @@ type DiaryViewProps = {
 export async function generateMetadata({
   params,
 }: DiaryViewProps): Promise<Metadata> {
+  const { id: diaryId } = await params;
+  const isoStr = convertDateIdToValidDate(diaryId);
+
+  if (!isoStr) {
+    return {
+      title: "My diary: invalid date | Birdiary",
+    };
+  }
+
+  const localeStr = createLocaleString(isoStr, "med");
   const username = await getUsername();
 
-  const diaryId = (await params).id;
-  const iso = convertDateIdToValidDate(diaryId) as string;
-  const date = createLocaleString(iso, "med");
-
   return {
-    title: `${username ? `${username}'s` : "My"} diary for ${date} | Birdiary`,
+    title: `${username ? `${username}'s` : "My"} diary on ${localeStr} | Birdiary`,
   };
 }
 
@@ -55,7 +61,7 @@ export default async function DiaryDetailsView({
         <ViewHeader
           backLinkHref="diary"
           backLinkText="Go to my diary"
-          headingText={`${username ? `${username}'s` : "My"} Birding Diary: ${createLocaleString(validDateId!, "med")}`}
+          headingText={`${username ? `${username}'s` : "My"} Birding Diary: ${validDateId ? createLocaleString(validDateId, "med") : "Invalid Date"}`}
         />
         {validDateId &&
         parsedPage &&
