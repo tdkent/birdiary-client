b271487d8bb0213fa17746687e5db63e1a855825
Tim Kent <timkentdev@gmail.com>
2025-12-13 13:32:08 -0800

    refactor: add basic callback functions to handle turnstile responses

--- components/pages/auth/Turnstile.tsx
+++ components/pages/auth/Turnstile.tsx
@@ -4,6 +4,15 @@ import Head from "next/head";
 import Script from "next/script";
 
 export default function Turnstile() {
+  function onSuccess(token: string) {
+    console.log(token);
+  }
+  function onError(errorCode: string) {
+    console.error(errorCode);
+  }
+  function onExpired() {
+    console.warn("Turnstile token expired");
+  }
   return (
     <>
       <Head>
@@ -15,7 +24,9 @@ export default function Turnstile() {
         data-sitekey={TURNSTILE_SITE_KEY}
         data-theme="light"
         data-size="normal"
-        data-callback="onSuccess"
+        data-callback={onSuccess}
+        data-error-callback={onError}
+        data-expired-callback={onExpired}
       ></div>
     </>
   );
