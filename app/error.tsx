005879f4d6d1409389539964991d0ac463b01c8b
Tim Kent <timkentdev@gmail.com>
2025-12-17 12:45:23 -0800

    feat: add reset btn and update msg props in error boundary component

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,29 @@
 "use client";
 
 import ErrorDisplay from "@/components/pages/shared/ErrorDisplay";
+import { Button } from "@/components/ui/button";
+import { Messages } from "@/models/api";
 
-export default function ErrorBoundary() {
-  return <ErrorDisplay />;
+type ErrorBoundaryProps = {
+  error: Error;
+};
+
+export default function ErrorBoundary({ error }: ErrorBoundaryProps) {
+  const msg =
+    error.message === "fetch failed"
+      ? Messages.ServerOutageError
+      : Messages.UnknownUnexpectedError;
+  return (
+    <>
+      <ErrorDisplay msg={msg} />
+      <Button
+        className="my-8"
+        onClick={() => window.location.reload()}
+        size="lg"
+        variant="secondary"
+      >
+        Reload
+      </Button>
+    </>
+  );
 }
