86b2c70988d802c3e8af3c1ea781d7cdee176eec
Tim Kent <timkentdev@gmail.com>
2025-12-28 12:28:14 -0800

    refactor(types): add api type to match updated server response

--- types/api.types.ts
+++ types/api.types.ts
@@ -1,1 +1,10 @@
+export type ApiResponse<T> = {
+  count: number;
+  data: T | T[];
+  error: boolean;
+  message: string;
+  statusCode: number;
+  timestamp: string;
+};
+
 export type RequestBody = { [key: string]: unknown };
