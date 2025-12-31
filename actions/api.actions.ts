7157abd8e3de54c58ab7f9ee70bb6a69a6fc5337
Tim Kent <timkentdev@gmail.com>
2025-12-30 21:03:22 -0800

    refactor(api): generate headers in server api request function

--- actions/api.actions.ts
+++ actions/api.actions.ts
@@ -1,18 +1,30 @@
 "use server";
 
 import CONFIG from "@/constants/config.constants";
+import { getCookie } from "@/helpers/auth";
 
 type ApiRequestInputs =
   | {
-      method?: never;
+      method?: "DELETE";
       requestBody?: never;
       route: string;
     }
   | {
-      method: "DELETE" | "PATCH" | "POST" | "PUT";
+      method: "PATCH" | "POST" | "PUT";
       requestBody: object;
       route: string;
     };
+
+type Headers = {
+  Authorization?: string;
+  "Content-Type"?: "application/json";
+};
+
+type RequestHeaders = {
+  body?: string;
+  headers?: Headers;
+  method?: "DELETE" | "PATCH" | "POST" | "PUT";
+};
 
 export async function serverApiRequest({
   method,
@@ -20,7 +32,22 @@ export async function serverApiRequest({
   requestBody,
 }: ApiRequestInputs) {
   console.log("Server-side api request at", new Date().toISOString());
+
   const url = CONFIG.BASE_URL + route;
-  const res = await fetch(url);
+
+  const requestHeaders: RequestHeaders = {};
+  const headers: Headers = {};
+
+  const jwt = await getCookie();
+
+  if (jwt) headers["Authorization"] = `Bearer ${jwt}`;
+  if (method) requestHeaders["method"] = method;
+  if (requestBody) {
+    headers["Content-Type"] = "application/json";
+    requestHeaders["body"] = JSON.stringify(requestBody);
+  }
+  if (Object.keys(headers).length) requestHeaders["headers"] = headers;
+
+  const res = await fetch(url, requestHeaders);
   return res.json();
 }
